ddb2ab3f2ada733a758c9c0f438b375b114a68d0
RugessNome <vincent.chambrin@gmail.com>
2018-07-28 13:46:37 +0200

    add InitializerList class

---
+++ include/script/initializerlist.h
@@ -0,0 +1,39 @@
+// Copyright (C) 2018 Vincent Chambrin
+// This file is part of the libscript library
+// For conditions of distribution and use, see copyright notice in LICENSE
+
+#ifndef LIBSCRIPT_INITIALIZER_LIST_H
+#define LIBSCRIPT_INITIALIZER_LIST_H
+
+#include "libscriptdefs.h"
+
+namespace script
+{
+
+class Value;
+
+class LIBSCRIPT_API InitializerList
+{
+private:
+  Value *begin_;
+  Value *end_;
+
+public:
+  InitializerList() : begin_(nullptr), end_(nullptr) { }
+  InitializerList(const InitializerList &) = default;
+  ~InitializerList() = default;
+
+  typedef Value* iterator;
+
+  inline iterator begin() const { return begin_; }
+  inline iterator end()  const { return end_; }
+
+  inline size_t size() const { return std::distance(begin_, end_); }
+
+  InitializerList & operator=(const InitializerList &) = default;
+};
+
+} // namespace script
+
+
+#endif // LIBSCRIPT_INITIALIZER_LIST_H
