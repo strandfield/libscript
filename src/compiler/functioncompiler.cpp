3fb6bbffb24ddc305451eb0483aa0cce871301d4
RugessNome <vincent.chambrin@gmail.com>
2018-09-02 11:23:32 +0200

    replace ConversionSequence by Conversion in FunctionCompiler (2/2)

--- src/compiler/functioncompiler.cpp
+++ src/compiler/functioncompiler.cpp
@@ -960,13 +960,14 @@ void FunctionCompiler::processVariableDeclaration(const std::shared_ptr<ast::Var
       var_type = var_type.withFlag(Type::ReferenceFlag);
   }
 
-  ConversionSequence seq = ConversionSequence::compute(value, var_type, engine());
-  if (seq == ConversionSequence::NotConvertible())
+  /// TODO: use Initialization instead
+  Conversion conv = Conversion::compute(value, var_type, engine());
+  if (conv == Conversion::NotConvertible())
     throw CouldNotConvert{ dpos(init), value->type(), var_type };
 
   /// TODO : this is not optimal I believe
   // we could add copy elision
-  value = ConversionProcessor::convert(engine(), value, var_type, seq);
+  value = ConversionProcessor::convert(engine(), value, conv);
   processVariableCreation(var_type, var_decl->name->getName(), value);
 }
 
