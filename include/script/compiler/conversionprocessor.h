27645c4945f77d55db734d7f0fc15b31d7e0d1c7
RugessNome <vincent.chambrin@gmail.com>
2018-09-02 14:00:31 +0200

    remove use of ConversionSequence in ConversionProcessor

--- include/script/compiler/conversionprocessor.h
+++ include/script/compiler/conversionprocessor.h
@@ -18,11 +18,6 @@ namespace compiler
 class ConversionProcessor
 {
 public:
-
-  static std::shared_ptr<program::Expression> sconvert(Engine *e, const std::shared_ptr<program::Expression> & arg, const Type & type, const StandardConversion & conv);
-  static std::shared_ptr<program::Expression> listinit(Engine *e, const std::shared_ptr<program::Expression> & arg, const Type & type, const std::shared_ptr<ListInitializationSequence> & linit);
-  static std::shared_ptr<program::Expression> convert(Engine *e, const std::shared_ptr<program::Expression> & arg, const Type & type, const ConversionSequence & conv);
-  static void prepare(Engine *e, std::vector<std::shared_ptr<program::Expression>> & args, const Prototype & proto, const std::vector<ConversionSequence> & conversions);
   static script::Type common_type(Engine *e, const std::shared_ptr<program::Expression> & a, const std::shared_ptr<program::Expression> & b);
 
   static std::shared_ptr<program::Expression> sconvert(Engine *e, const std::shared_ptr<program::Expression> & arg, const StandardConversion2 & conv);
